423b7307a63474b307b1658dcb9386acbea608ed
CHOILOCAL <giants1217@naver.com>
2023-10-16 20:28:06 +0900

    add a CallBack Func

---
+++ OCT/Callback.js
@@ -0,0 +1,16 @@
+// 콜백(Callback)
+// 함수의 인수로 사용되는 함수
+
+// setTimeout(함수, 시간)
+
+function timeout(cb) {
+    setTimeout(() => {
+        console.log('CHOILOCAL');
+        // console.log('Done!');
+        cb()
+    }, 3000)
+}
+
+timeout(() => {
+    console.log('Done!')
+});
